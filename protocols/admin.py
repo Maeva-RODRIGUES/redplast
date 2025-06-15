f22598f7973d762245908f98417424e705fc628f
Maeva RODRIGUES <maeva.rodrigues@lapiscine.pro>
2025-06-15 19:10:22 +0200

    ✨ New Feature:  register Protocol model with custom admin view

--- protocols/admin.py
+++ protocols/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-
-# Register your models here.
 from .models import Protocol
 
-admin.site.register(Protocol)
+@admin.register(Protocol)
+class ProtocolAdmin(admin.ModelAdmin):
+    list_display = ("title", "author", "created_at")  
+    search_fields = ("title", "author")
+    list_filter = ("created_at",)
+
